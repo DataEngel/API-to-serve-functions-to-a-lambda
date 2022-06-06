4d7d8ca2c352f955fffaae746aabbb24b888708c
DataEngel <miguel.program.73@gamil.com>
2022-06-05 20:40:06 -0500

    All code is working

--- routes/user.py
+++ routes/user.py
@@ -3,6 +3,8 @@ from config.db import conn
 from models.user import predictions
 from schemas.user import User
 from typing import List
+from lambda_in.inboke_lambda_docker import inboke_lambda_1
+import json
 
 user = APIRouter()  
 
@@ -41,5 +43,35 @@ def create_user(user: User):
     description="Get a single user by Id",
 )
 
+def get_user_one(id: str):
+    return conn.execute(predictions.select().where(predictions.c.id == id)).first() 
+
+@user.get(
+    "/send_by_id/{id}",
+    tags=["users"],
+    response_model=User,
+    description="send a single user by Id",
+)
 def get_user(id: str):
-    return conn.execute(predictions.select().where(predictions.c.id == id)).first()
+    query = conn.execute(predictions.select().where(predictions.c.id == id)).first()
+    values_query = query 
+
+    id_ = values_query.id
+    id_main = str(id_) 
+
+    inputParamss = {
+        id_main : {
+            "1": values_query.age,
+            "2": values_query.years_on_the_job,
+            "3": values_query.nb_previous_loans,
+            "4": values_query.avg_amount_loans_previous,
+            "5": values_query.flag_own_car
+        }
+    }
+    
+    lambda_returns = inboke_lambda_1(inputParamss)
+
+    print("Lambda returns: ", lambda_returns)
+
+    return query
+
